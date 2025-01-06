059a1dfc5cb1a0906aeb6505254802646a385814
potapchukdmytro <traiget@gmail.com>
2025-01-06 17:06:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const App = () => {
+    const title = "First react project";
+
+    return (
+        <>
+            <div className="b-color">
+                <h1>{title}</h1>
+            </div>
+            <div className="Content">
+                <p>Content</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
